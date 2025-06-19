55757e4d0d1e0b6e0c5bb56b5296e188795201e4
jupsa <keith.albert@gmail.com>
2025-06-19 17:21:06 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
 const express = require('express');
 const axios = require('axios');
-const cheerio = require('cheerio');
 const app = express();
 
 app.get('/standings-embed', async (req, res) => {
   try {
-    const response = await axios.get('https://www.mlb.com/standings/');
-    const $ = cheerio.load(response.data);
-    const table = $('.section-content table').first().toString();
-    res.send(`
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <title>MLB Standings</title>
-        </head>
-        <body>
-          ${table}
-        </body>
-      </html>
-    `);
+    const { records } = (await axios.get('https://statsapi.mlb.com/api/v1/standings')).data;
+
+    let html = '<table border="1" cellpadding="5"><tr><th>Division</th><th>Team</th><th>W</th><th>L</th><th>PCT</th><th>GB</th></tr>';
+    records.forEach(div => {
+      const divisionName = div.division.name;
+      div.teamRecords.forEach(team => {
+        const { team: t, leagueRecord, gamesBack } = team;
+        html += `<tr>
+          <td>${divisionName}</td>
+          <td>${t.name}</td>
+          <td>${leagueRecord.wins}</td>
+          <td>${leagueRecord.losses}</td>
+          <td>${leagueRecord.pct}</td>
+          <td>${gamesBack}</td>
+        </tr>`;
+      });
+    });
+    html += '</table>';
+    res.send(`<html><body>${html}</body></html>`);
   } catch (err) {
-    res.status(500).send('Error loading standings');
+    console.error(err);
+    res.status(500).send('Error fetching standings');
   }
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server up on port ${PORT}`));

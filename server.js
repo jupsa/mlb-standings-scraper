a0b5a45041f7870ff07314fb2d2d8153a82d2020
jupsa <keith.albert@gmail.com>
2025-06-19 17:26:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,31 +3,18 @@ const axios = require('axios');
 const app = express();
 
 app.get('/standings-embed', async (req, res) => {
-  try {
-    const { records } = (await axios.get('https://statsapi.mlb.com/api/v1/standings')).data;
-
-    let html = '<table border="1" cellpadding="5"><tr><th>Division</th><th>Team</th><th>W</th><th>L</th><th>PCT</th><th>GB</th></tr>';
-    records.forEach(div => {
-      const divisionName = div.division.name;
-      div.teamRecords.forEach(team => {
-        const { team: t, leagueRecord, gamesBack } = team;
-        html += `<tr>
-          <td>${divisionName}</td>
-          <td>${t.name}</td>
-          <td>${leagueRecord.wins}</td>
-          <td>${leagueRecord.losses}</td>
-          <td>${leagueRecord.pct}</td>
-          <td>${gamesBack}</td>
-        </tr>`;
-      });
+  const resp = await axios.get('https://statsapi.mlb.com/api/v1/standings?leagueId=103');
+  const { records } = resp.data;
+  
+  let html = '<table border="1"><tr><th>Div<\/th><th>Team<\/th><th>W<\/th><th>L<\/th><th>PCT<\/th><th>GB<\/th><\/tr>';
+  records.forEach(div => {
+    div.teamRecords.forEach(tm => {
+      html += `<tr><td>${div.division.name}<\/td><td>${tm.team.name}<\/td><td>${tm.leagueRecord.wins}<\/td><td>${tm.leagueRecord.losses}<\/td><td>${tm.leagueRecord.pct}<\/td><td>${tm.gamesBack}<\/td><\/tr>`;
     });
-    html += '</table>';
-    res.send(`<html><body>${html}</body></html>`);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error fetching standings');
-  }
+  });
+  html += '</table>';
+
+  res.send(`<html><body>${html}<\/body><\/html>`);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
+app.listen(process.env.PORT || 3000);
